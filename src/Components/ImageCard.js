4fbd9914da2d481942deac2b2404dc30c16695d4
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-10-09 16:18:31 -0600

    added single card that loads today's image

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -4,6 +4,9 @@ const ImageCard = props => {
     return (
         <div className="image-of-the-day">
             <h2>{props.title}</h2>
+            <h3>{props.date}</h3>
+            <img src={props.url}></img>
+            <p>{props.explanation}</p>
         </div>
     )
 }
--- src/Components/NasaImage.js
+++ src/Components/NasaImage.js
@@ -3,20 +3,20 @@ import axios from "axios";
 import ImageCard from './ImageCard'
 
 export default function NasaStuff() {
-    const [title, setTitle] = useState([]);
+    const [info, setInfo] = useState([]);
 
     useEffect(() => {
         axios
             .get('https://api.nasa.gov/planetary/apod?api_key=2QILodeZ0FXikkfaJQefOTHCrk3zmebbSCGdeeog')
             .then( res => {
                 console.log("Here's the stuff from NASA: ", res);
-                setTitle(res.data.title);
+                setInfo(res.data);
             })
             .catch( err => {
                 console.log("The NASA data was not returned", err);
             });
     }, []);
     return (
-        <ImageCard title={title}/>
+        <ImageCard title={info.title} date={info.date} url={info.url} explanation={info.explanation}/>
     )
 }
