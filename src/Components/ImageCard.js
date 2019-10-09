ea3bd28663bcaad35c4fcf9a2d262cc388355ef4
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-10-09 16:43:33 -0600

    added alt prop to img on ImageCard

--- src/Components/ImageCard.js
+++ src/Components/ImageCard.js
@@ -5,7 +5,7 @@ const ImageCard = props => {
         <div className="image-of-the-day">
             <h2>{props.title}</h2>
             <h3>{props.date}</h3>
-            <img src={props.url}></img>
+            <img src={props.url} alt=""></img>
             <p>{props.explanation}</p>
         </div>
     )
